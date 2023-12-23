92f2a8ddee2e0d957f792d91d77b7f7ea1dee86b
Rahul <prathampathak14@gmail.com>
2023-12-24 03:21:14 +0530

    Week-4 DOM

---
+++ week-4/week-4.1/script.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+//dom (document)
+//need implemnetd in runtime browser & not in ECMA specification
+/* 
+document-> Is DOM(it has wierd doc)
+but this gives you a bunch of functions-> is effectively what DOM is 
+& is what we are going to manipulate the DOM
+It is an API provied by browser(V8 engine)-> we use to modify HTMl
+In tree -> document is at the root
+
+Window-> document(which consist HTMl representation) -> call DOM
+*/
