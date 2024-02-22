ac369115f1f9a69de3756f19a17e7e5d28cb1e13
Rahul <prathampathak14@gmail.com>
2024-02-22 22:29:20 +0530

    NextJs

--- ad-hoc/another-one/src/app/chai/page.tsx
+++ ad-hoc/another-one/src/app/chai/page.tsx
@@ -1,6 +1,6 @@
 export default function ChaiPage(){
     return(
-        <h1 className="text-3xl text-center">Chai Page</h1>
+        <h1 className="text-3xl text-center mt-8">Chai Page</h1>
     )
 }
 
