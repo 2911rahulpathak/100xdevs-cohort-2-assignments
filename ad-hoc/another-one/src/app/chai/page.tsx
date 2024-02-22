c30cac4d6d3660fa097ce8b3bd42c6566a711688
Rahul <prathampathak14@gmail.com>
2024-02-23 00:01:51 +0530

    nextJs

--- ad-hoc/another-one/src/app/chai/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ChaiPage(){
-    return(
-        <h1 className="text-3xl text-center mt-8">Chai Page</h1>
-    )
-}
-
-//here the rules of framework came into Picture as file name we changed from chai.tsx to page.tsx
-//33min
